9da378a845569eee6851be8d4b86cc1b05be2882
Walter dos Santos <walter4asantos@gmail.com>
2020-10-29 13:48:11 -0300

    DepartmentService

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -1,9 +1,12 @@
 package gui;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -12,9 +15,12 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import model.entities.Department;
+import model.services.DepartmentService;
 
 public class DepartmentListController implements Initializable {
 
+	private DepartmentService service;
+	
 	@FXML
 	private TableView<Department> tableViewDepartment; //Criando as referências para meus objetos
 	
@@ -27,9 +33,15 @@ public class DepartmentListController implements Initializable {
 	@FXML
 	private Button btNew; //Criando as referências para meus objetos
 	
+	private ObservableList<Department> obsList;
+	
 	@FXML
 	public void onBtNewAction() {  //Método do tratamento de eventos do click desse botão
 		System.out.println("onBtNewAction");
+	}
+	
+	public void setDepartmentService(DepartmentService service) { //método set para evitar fazer acoplamento forte
+		this.service = service;	  								  // Dessa forma injetamos dependencia por outro lugar
 	}
 	
 	@Override
@@ -45,5 +57,14 @@ public class DepartmentListController implements Initializable {
 		//Massete para fazer minha tableView acompanhar a minha janela
 		Stage stage = (Stage) Main.getMainScene().getWindow(); //Pegando a referencia da minha janela e fazendo o downcasting para Stage
 		tableViewDepartment.prefHeightProperty().bind(stage.heightProperty());
+	}
+	
+	public void updateTableView() {
+		if (service == null) {
+			throw new IllegalStateException("Service was null");
+		}
+		List<Department> list = service.findAll();
+		obsList = FXCollections.observableArrayList(list);
+		tableViewDepartment.setItems(obsList);
 	}
 }
